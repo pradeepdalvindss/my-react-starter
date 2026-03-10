f0105ca88f2c434dd4d4d3e556281b1449367dbf
pradeepdalvindss <pradeep.dalv@ndsofttech.com>
2026-03-10 17:52:56 +0530

    moved app.jsx into components folder

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from '../assets/react.svg'
+// import viteLogo from '../vite.svg'
+ 
+import '../App.css'
 
 function App() {
   const handleClick = () => {
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Hello, Pradeep!</h1>
+      <h1>Hello, Pradeep Dalvi!</h1>
       <button onClick={handleClick} style={{ padding: "10px 20px", fontSize: "16px" }}>
         Click Me
       </button>
